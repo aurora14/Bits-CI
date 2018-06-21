eaee154f44216aeab24f8c79c3e8712a7ba9f9b7
Alexei <Gudimenko>
2018-06-21 16:23:50 +1000

    Added workspace data to version control for Bitrise compliance

---
+++ bitrise unofficial ios/Services/API/BRSessionManager.swift
@@ -0,0 +1,9 @@
+//
+//  BRSessionManager.swift
+//  bitrise unofficial ios
+//
+//  Created by Alexei Gudimenko on 18/6/18.
+//  Copyright © 2018 Alexei Gudimenko. All rights reserved.
+//
+
+import Foundation
