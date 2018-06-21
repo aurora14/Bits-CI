1ddb21885c6397ba118e74106433f4b4ba91ad8a
Alexei <Gudimenko>
2018-06-21 17:20:23 +1000

    Added custom session manager for handling background network calls

--- bitrise unofficial ios/Services/API/BRSessionManager.swift
+++ bitrise unofficial ios/Services/API/BRSessionManager.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2018 Alexei Gudimenko. All rights reserved.
 //
 
+
 import Foundation
+import Alamofire
+
+class BRSessionManager {
+  
+  static let shared = BRSessionManager()
+  
+  private init() { }
+  
+  private func _httpSessionManager() -> Alamofire.SessionManager {
+    
+    // Create custom manager
+    let configuration = URLSessionConfiguration
+      .background(withIdentifier: "com.gudimenko.alexei.bitrise-unofficial-ios")
+    configuration.httpAdditionalHeaders = Alamofire.SessionManager.defaultHTTPHeaders
+    let manager = Alamofire.SessionManager(
+      configuration: configuration
+      //serverTrustPolicyManager: ServerTrustPolicyManager(policies: serverTrustPolicies)
+    )
+    
+    return manager
+  }
+  
+  public var httpSessionManager: Alamofire.SessionManager {
+    return _httpSessionManager()
+  }
+}
