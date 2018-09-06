61484c0377bfabeaf245e504cf90ea0c232ff3aa
Alexei <Gudimenko>
2018-09-07 00:49:09 +1000

    Switched off Fabric debug logs to clear up some space in the console

--- bitrise unofficial ios/Shared/AppDelegate+Utils.swift
+++ bitrise unofficial ios/Shared/AppDelegate+Utils.swift
@@ -24,7 +24,7 @@ extension AppDelegate {
   /// your "finishedLaunchingWith(options:)", just before "return true".
   func initReportingUtils() {
     FirebaseApp.configure()
-    Fabric.sharedSDK().debug = true
+    Fabric.sharedSDK().debug = false
     Fabric.with([Crashlytics.self])
   }
   
