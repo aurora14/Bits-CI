bef263fb0d24441c5783bd905b29459baaf817dd
Alexei <Gudimenko>
2018-10-03 20:49:04 +1000

    If the project viewmodel doesn't have an ID attached to it, the builds are no longer fetched

--- bitrise unofficial ios/ViewModels/BitriseProjectVM.swift
+++ bitrise unofficial ios/ViewModels/BitriseProjectVM.swift
@@ -102,7 +102,12 @@ class BitriseProjectViewModel: CellRepresentable {
   
   init(with app: BitriseApp) {
     self.app = app
-    // get last build info here?
+    
+    guard !app.slug.isEmpty else {
+      // if there's no app ID, don't get the last builds (e.g. if loading dummy items)
+      return
+    }
+    
     updateLastBuild()
     DispatchQueue.global(qos: .background).async { [weak self] in
       self?.updateAllBuilds()
