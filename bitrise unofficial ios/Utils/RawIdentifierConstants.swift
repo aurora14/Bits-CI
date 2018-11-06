5e1ad8a7d701562b554e01697e1d4a6d422792cd
Alexei <Gudimenko>
2018-11-06 16:24:35 +1100

    Added a file to maintain a list of view identifier constants - these are items that must exist as globals one way or another in order for them to work as cell identifiers and other components that function in similar ways. However should explore whether it can be moved to something like SwiftGen's script generation

---
+++ bitrise unofficial ios/Utils/RawIdentifierConstants.swift
@@ -0,0 +1,11 @@
+//
+//  RawIdentifierConstants.swift
+//  bitrise unofficial ios
+//
+//  Created by Alexei Gudimenko on 6/11/18.
+//  Copyright © 2018 Alexei Gudimenko. All rights reserved.
+//
+
+import Foundation
+
+let kProjectCellReusableIdentifier = "ProjectCell"
