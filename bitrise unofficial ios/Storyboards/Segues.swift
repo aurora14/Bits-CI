0c914438b684320db8a954e5cf3c19cd01854609
Alexei <Gudimenko>
2018-11-21 13:35:48 +1100

    Temp save point (localisation, segues, storyboard mods)

--- bitrise unofficial ios/Storyboards/Segues.swift
+++ bitrise unofficial ios/Storyboards/Segues.swift
@@ -13,6 +13,7 @@ import UIKit
 // swiftlint:disable explicit_type_interface identifier_name line_length type_body_length type_name
 internal enum StoryboardSegue {
   internal enum Main: String, SegueType {
+    case acknowledgementSegue = "AcknowledgementSegue"
     case buildDetailSegue = "BuildDetailSegue"
     case getNewTokenSegue = "GetNewTokenSegue"
     case profileTabTokenSegue = "ProfileTabTokenSegue"
