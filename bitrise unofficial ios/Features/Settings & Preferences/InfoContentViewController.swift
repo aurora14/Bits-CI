ffa58cf6207117041cc8f227f553db940a04d46a
Alexei <Gudimenko>
2018-11-21 13:34:42 +1100

    Markdown presenting controller that currently handles showing Acknowledgements, but can also be used for other textual presentation where using a normal textview or webview is impractical

--- bitrise unofficial ios/Features/Settings & Preferences/InfoContentViewController.swift
+++ bitrise unofficial ios/Features/Settings & Preferences/InfoContentViewController.swift
@@ -7,24 +7,58 @@
 //
 
 import UIKit
+import MarkdownView
+import SafariServices
+
+enum InfoPageSelector {
+  case acknowledgements
+}
 
 class InfoContentViewController: UIViewController {
+  
+  @IBOutlet weak var markdownContentView: MarkdownView!
+  
+  private var content: String = ""
+  
+  var pageSelector: InfoPageSelector = .acknowledgements
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    setupMarkdownView()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    loadContentFromFile()
+  }
+  
+  private func setupMarkdownView() {
+    markdownContentView.onTouchLink  = { [weak self] request in
+      guard let url = request.url, url.scheme == "https" else {
+        return false
+      }
+      
+      let safariVC = SFSafariViewController(url: url)
+      safariVC.preferredControlTintColor = Asset.Colors.bitriseGreen.color
+      self?.present(safariVC, animated: true, completion: nil)
+      return true
+    }
+  }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  private func loadContentFromFile() {
+    
+    guard let path = Bundle.main.path(forResource: "Acknowledgements", ofType: ".md") else {
+      print("Failed to get markdown file at specified path")
+      return
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    do {
+      let contentString = try String(contentsOfFile: path, encoding: .utf8)
+      //print(contentString)
+      markdownContentView.load(markdown: contentString)
+    } catch let error {
+      print("Markdown String parsing error: \(error.localizedDescription)")
     }
-    */
-
+  }
 }
