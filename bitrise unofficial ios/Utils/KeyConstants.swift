1d19659d64470b079b0ed7c5c20a524e3399a0e2
Alexei <Gudimenko>
2018-12-03 17:58:34 +1100

    Expanded and standardised constants files. Keys and Identifiers have been separated. Next step may be abstracting them to an enum.

---
+++ bitrise unofficial ios/Utils/KeyConstants.swift
@@ -0,0 +1,16 @@
+//
+//  KeyConstants.swift
+//  bitrise unofficial ios
+//
+//  Created by Alexei Gudimenko on 21/11/18.
+//  Copyright © 2018 Alexei Gudimenko. All rights reserved.
+//
+//  Maintains a list of UserDefaults keys.
+//  This is separate from the L10n constants because L10n is primarily kept for localization work, and keys shouldn't be translated
+
+import Foundation
+
+internal let kBackgroundTimeValue = "ApplicationBackgroundTime"
+internal let kPasscodeTimeoutEnabled = "PasscodeTimeoutEnabled"
+internal let kPasscodeTimeoutValue = "PasscodeTimeoutValue"
+internal let kPasscodeUnlockKey = "PasscodeUnlockKey"
