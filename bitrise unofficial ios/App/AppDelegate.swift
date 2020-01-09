5c1d2f8fedb14b36a6800df4e9b817fc12533e53
Alexei <alexei.gudimenko@gmail.com>
2020-01-09 16:02:41 +1100

    Tech: cleaned up AppDelegate from unused imports and template methods

--- bitrise unofficial ios/App/AppDelegate.swift
+++ bitrise unofficial ios/App/AppDelegate.swift
@@ -8,18 +8,11 @@
 
 
 import UIKit
-import Fabric
-import Crashlytics
-import Firebase
-import Alamofire
-import AlamofireNetworkActivityIndicator
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-  
+final class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
-
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions
     launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -37,24 +30,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     recordTimeWhenMovedToBackground()
   }
 
-  func applicationDidEnterBackground(_ application: UIApplication) {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    
-    // TODO: - save the time when the application became inactive. This will be used by the screen locking method
-  }
-
-  func applicationWillEnterForeground(_ application: UIApplication) {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-  }
-
   func applicationDidBecomeActive(_ application: UIApplication) {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     
     unlockIfNecessary()
-  }
-
-  func applicationWillTerminate(_ application: UIApplication) {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
   }
 }
